eeff1e0fbebf2397d43bc7c60a74f4863e054494
JyotiDixit172 <jyotidixit172@gmail.com>
2026-01-23 02:57:21 +0530

    Backend Learnings Day 1

---
+++ nodejs/1_fundamentals/2_arithmetic.js
@@ -0,0 +1,4 @@
+console.log(2+2);
+console.log("hello world");
+let x=30;
+console.log(x);
---
+++ nodejs/1_fundamentals/3_repl.js
@@ -0,0 +1,33 @@
+// REPL - Read, Evaluate, Print, Loop
+// A simple example of using Node.js REPL module
+
+// R - Read the user input
+// E - Evaluate the user input
+// P - Print or output the result
+// L - return and wait wait for new input
+
+// So this REPL environment allows us to execute any JS expression, any JS code outside of the browser.
+
+/* 
+JS automatically detects data types. It's dynamically typed:
+variables can hold nay data type without declaration.
+The type is inferred from the assigned value.
+Types can be changeed during runtime.
+This above dynamic typing cabn be verified using typeof operator.
+*/ 
+
+// User object = {property:value}
+let user = {name:"John", age:28}
+console.log(user.name);
+console.log(user.age);
+
+
+//  Why don't we use REPL for development purpose ?
+
+/* 
+In terminal, during a single REPL session, all the variables, objects created uring that time are
+stored during that session only, and gets flushed out in case of cntrl+c, which means 
+we are out of node.js environment
+*/
+
+//  _ (underscore) - is a special variable which stores the result of the previous expression.
